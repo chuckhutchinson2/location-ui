252529a46bae48c31344d59553d8516b15288d52
chuck hutchinson <chuckhutchinson2@yahoo.com>
2017-04-16 22:18:37 -0400

    added react-center

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Center from 'react-center';
 import LocationComponent from './LocationComponent.jsx';
 import TimerComponent from './TimerComponent.jsx';
 
 class App extends React.Component {
   render () {
-    return (<LocationComponent />)
+    return (	<Center>
+    				<LocationComponent />
+    			</Center>
+    )
   }
 }
+
 
 render(<App/>, document.getElementById('root'));
