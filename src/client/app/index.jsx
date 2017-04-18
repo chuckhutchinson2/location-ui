bf496601357fb39f1565faab77c2be5c6b8b0216
chuck hutchinson <chuckhutchinson2@yahoo.com>
2017-04-18 09:49:58 -0400

    added tab control

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -3,13 +3,33 @@ import {render} from 'react-dom';
 import Center from 'react-center';
 import LocationComponent from './LocationComponent.jsx';
 import TimerComponent from './TimerComponent.jsx';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class App extends React.Component {
+  handleSelect(index, last) {
+    	console.log('Selected tab: ' + index + ', Last tab: ' + last);
+  }
+  
   render () {
-    return (	<Center>
-    				<LocationComponent />
-    			</Center>
-    )
+    return (
+    	<Tabs
+        	onSelect={this.handleSelect}
+        	selectedIndex={2}
+      		>
+
+			<TabList>
+				<Tab>Location</Tab>
+				<Tab>Location2</Tab>
+			</TabList>
+			
+			<TabPanel>
+    			<LocationComponent />
+    		</TabPanel>
+ 			<TabPanel>
+    			<LocationComponent />
+    		</TabPanel>   				
+    	</Tabs>
+    	)
   }
 }
 
