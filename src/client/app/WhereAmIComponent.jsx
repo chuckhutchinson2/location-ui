c51cf80c2792d3e1bc66a2401763f029a7f6ebf8
chuck hutchinson <chuckhutchinson2@yahoo.com>
2017-05-12 18:57:38 -0400

    added style

--- src/client/app/WhereAmIComponent.jsx
+++ src/client/app/WhereAmIComponent.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
-
 import WhatIsMyLocationApi from './WhatIsMyLocationApi.jsx';
+import styler from 'react-styling'
+
+const style = styler
+`
+	table:
+		padding: 18px
+		margin: 20px
+		border: 1px solid black
+		
+	td:
+		padding: 5px
+		margin: 5px
+		border: 1px solid black
+`
 
 export default class WhereAmIComponent extends React.Component {
 
@@ -27,18 +40,38 @@ export default class WhereAmIComponent extends React.Component {
 
   render() {
     return (
-    	<div>
-	    	<div>IP: {this.state.location.ip}</div>
-	   		<div>country_code: {this.state.location.country_code}</div>
-	   		<div>region_code: {this.state.location.region_code}</div>
-	   		<div>region_name: {this.state.location.region_name}</div>
-	   		<div>city: {this.state.location.city}</div>
-	   		<div>zip_code: {this.state.location.zip_code}</div>
-	   		<div>time_zone: {this.state.location.time_zone}</div>
-	   		<div>latitude: {this.state.location.latitude}</div>
-	   		<div>longitude: {this.state.location.longitude}</div>
-	   		<div>metro_code: {this.state.location.metro_code}</div>
-		</div>
+    	<table style={style.table}>
+    		<tr>
+	    		<td style={style.td}>IP</td><td style={style.td}>{this.state.location.ip}</td>
+	    	</tr>
+    		<tr>
+	 	  		<td style={style.td}>country_code</td><td style={style.td}>{this.state.location.country_code}</td>
+	    	</tr>
+    		<tr>
+		   		<td style={style.td}>region_code</td><td style={style.td}>{this.state.location.region_code}</td>
+	    	</tr>
+    		<tr>
+		   		<td style={style.td}>region_name</td><td style={style.td}>{this.state.location.region_name}</td>
+	    	</tr>
+    		<tr>
+		   		<td style={style.td}>city</td><td style={style.td}>{this.state.location.city}</td>
+	    	</tr>
+    		<tr>
+		   		<td style={style.td}>zip_code</td><td style={style.td}>{this.state.location.zip_code}</td>
+	    	</tr>
+    		<tr>
+		   		<td style={style.td}>time_zone</td><td style={style.td}>{this.state.location.time_zone}</td>
+	    	</tr>
+    		<tr>
+		   		<td style={style.td}>latitude</td><td style={style.td}>{this.state.location.latitude}</td>
+	    	</tr>
+    		<tr>
+		   		<td style={style.td}>longitude</td><td style={style.td}>{this.state.location.longitude}</td>
+	    	</tr>
+    		<tr>
+		   		<td style={style.td}>metro_code</td><td style={style.td}>{this.state.location.metro_code}</td>
+	    	</tr>
+		</table>
     	
     	);
   }
